a3284db782a8735265e25a16e153f25e7a134e28
DeppySon-I <deppy.sontani@student.unsil.ac.id>
2014-05-04 16:06:36 +0700

    Create style.css

---
+++ style.css
@@ -0,0 +1,24 @@
+body {
+	font-family: Verdana, Helvetica, Arial, sans-serif;
+	margin: 0;
+	padding: 0;
+}
+
+
+a {
+	cursor: pointer;
+	color: #333;
+	text-decoration: none;
+}
+
+
+a:hover {
+	cursor: pointer;
+	color: #1b7fcc;
+	text-decoration: underline;
+
+}
+
+.dur {
+	font: Verdana;
+}
